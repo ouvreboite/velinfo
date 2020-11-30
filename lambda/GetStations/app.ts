aec75f10ec0e233f3d5c5d9c8a3850ae124c6ae9
Jean-Baptiste Muscat <muscatjb@gmail.com>
2020-11-30 23:27:22 +0100

    feat: GET /stations lambda + httpApi

---
+++ lambda/GetStations/app.ts
@@ -0,0 +1,47 @@
+import "reflect-metadata";
+import { Status } from "../../common/domain";
+import { getAvailabilities } from "../../common/repository/availabilitiesDynamoRepository";
+import { getStationsStates } from "../../common/repository/stationsStatesRepository";
+
+export const lambdaHandler = async () => {
+    let [availabilities, stationStates] = await Promise
+        .all([getAvailabilities(), getStationsStates()])
+
+
+    let stations = {};
+    for (const [stationCode, availability] of availabilities.byStationCode) {
+        let station = new Station();
+        station.code = stationCode;
+        station.name = availability.name;
+        station.capacity = availability.capacity;
+        station.electrical = availability.electrical;
+        station.mechanical = availability.mechanical;
+        station.coldSince = availability.coldSince;
+        if(stationStates.byStationCode.has(stationCode) 
+            && stationStates.byStationCode.get(stationCode).coldSince == availability.coldSince){
+            station.state = stationStates.byStationCode.get(stationCode).status;
+        }else{
+            station.state = Status.Ok;
+        }
+        stations[stationCode] = station;
+    }
+    
+    return {
+        fetchDateTime: availabilities.fetchDateTime,
+        mostRecentOfficialDueDateTime: availabilities.mostRecentOfficialDueDateTime,
+        stations: stations
+      }
+
+}
+
+class Station{
+    code: string;
+    name: string;
+    latitude: number;
+    longitude: number;
+    state: Status;
+    capacity: number;
+    electrical: number;
+    mechanical: number;
+    coldSince: Date;
+}
