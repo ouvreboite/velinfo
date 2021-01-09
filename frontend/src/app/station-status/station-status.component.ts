b384d889adf7c985753a92b756cb49f2b0610693
Jean-Baptiste Muscat <muscatjb@gmail.com>
2021-01-09 16:19:02 +0100

    refactor: extract status hadnlign in dedicated service

--- frontend/src/app/station-status/station-status.component.ts
+++ frontend/src/app/station-status/station-status.component.ts
@@ -1,20 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Station, OfficialStatus, State } from '../current-stations.service';
+import { StationStatusService } from '../station-status.service';
 
 @Component({
   selector: 'app-station-status',
   templateUrl: './station-status.component.html',
   styleUrls: ['./station-status.component.css']
 })
-export class StationStatusComponent implements OnInit {
+export class StationStatusComponent implements OnInit, OnChanges  {
  
   detailCSSClass: string;
+  statusEmoji: string;
+  statusText: string;
   @Input() station: Station;
   @Input() forceDetails: boolean = false;
   
-  constructor() { }
+  constructor( private stationStatusService: StationStatusService) { }
 
   ngOnInit() {
+    this.statusEmoji = this.stationStatusService.getStatusEmoji(this.station);
+    this.statusText = this.stationStatusService.getStatusText(this.station);
     this.detailCSSClass = this.forceDetails?'force-details':'hiddable-details';
+  }
+
+  ngOnChanges() {
+    this.ngOnInit();
   }
 }
