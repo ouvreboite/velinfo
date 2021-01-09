708762948a9dcc64db316987dad5d2da1c2f5ba1
Jean-Baptiste Muscat <muscatjb@gmail.com>
2021-01-10 00:43:28 +0100

    feat: add cachign to current-stations-service

--- frontend/src/app/current-stations.service.ts
+++ frontend/src/app/current-stations.service.ts
@@ -1,31 +1,63 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import { fr } from 'date-fns/locale'
+import differenceInSeconds from 'date-fns/differenceInSeconds';
+import { fr } from 'date-fns/locale';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentStationsService {
+  private configUrl = 'https://api.velinfo.fr/stations';
+  private loadTimestamp: Date;
+  private currentStations: CurrentStations;
+  private observable: Observable<CurrentStations>; 
 
   constructor(private http: HttpClient) {
   }
 
-  configUrl = 'https://api.velinfo.fr/stations';
-
   getStations(): Observable<CurrentStations> {
-    return this.http.get<CurrentStations>(this.configUrl)
+    this.invalidCacheIfNecessary()
+
+    if (this.currentStations) {
+      return of(this.currentStations);
+    } else if (this.observable) {
+      return this.observable;
+    } else {
+      this.observable = this.fetchStations()
       .pipe(
-        map(stations => this.mapDateAttributes(stations)),
-        map(stations => this.roundExpectedActivity(stations)),
-        map(stations => this.addLastActivityAgo(stations)),
-        map(stations => this.addOccupation(stations)),
-      )
+        map(stations => {
+          this.observable = null;
+          this.currentStations = stations;
+          return stations;
+        }));
+      return this.observable;
+    }
   }
 
-  mapDateAttributes(currentStations : CurrentStations): CurrentStations {
+  private fetchStations(): Observable<CurrentStations>{
+    console.log("fetchStations");
+    this.loadTimestamp = new Date();
+    return this.http.get<CurrentStations>(this.configUrl)
+    .pipe(
+      map(stations => this.mapDateAttributes(stations)),
+      map(stations => this.roundExpectedActivity(stations)),
+      map(stations => this.addLastActivityAgo(stations)),
+      map(stations => this.addOccupation(stations)),
+    )
+  }
+
+  private invalidCacheIfNecessary(){
+    if(!this.loadTimestamp || differenceInSeconds(new Date(), this.loadTimestamp) > 60 ){
+      this.loadTimestamp = null;
+      this.currentStations = null;
+      this.observable = null;
+    }
+  }
+
+  private mapDateAttributes(currentStations : CurrentStations): CurrentStations {
     currentStations.stations.forEach(station => {
       station.lastActivity = station.lastActivity?new Date(station.lastActivity): null;
     });
@@ -33,7 +65,7 @@ export class CurrentStationsService {
     return currentStations;
   }
 
-  addLastActivityAgo(currentStations: CurrentStations): CurrentStations{
+  private addLastActivityAgo(currentStations: CurrentStations): CurrentStations{
     let now = new Date();
     currentStations.stations.forEach(station => {
       station.lastActivity = station.coldSince?new Date(station.coldSince):now;
@@ -42,14 +74,14 @@ export class CurrentStationsService {
     return currentStations;
   }
 
-  addOccupation(currentStations: CurrentStations): CurrentStations{
+  private addOccupation(currentStations: CurrentStations): CurrentStations{
     currentStations.stations.forEach(station => {
       station.occupation = (station.electrical+station.mechanical)/station.capacity;
     });
     return currentStations;
   }
 
-  roundExpectedActivity(currentStations: CurrentStations): CurrentStations{
+  private roundExpectedActivity(currentStations: CurrentStations): CurrentStations{
     currentStations.stations.forEach(station => {
       station.expectedActivity = Math.round(station.expectedActivity);
     });
