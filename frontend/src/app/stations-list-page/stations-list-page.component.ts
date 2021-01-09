894b370569f4e918aff2ca9963a56ffcc5501479
Jean-Baptiste Muscat <muscatjb@gmail.com>
2021-01-10 00:43:57 +0100

    feat: use new stations-table comp in stations list page

--- frontend/src/app/stations-list-page/stations-list-page.component.ts
+++ frontend/src/app/stations-list-page/stations-list-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { CurrentStations, CurrentStationsService, Station } from '../current-stations.service';
 
@@ -9,74 +8,22 @@ import { CurrentStations, CurrentStationsService, Station } from '../current-sta
   styleUrls: ['./stations-list-page.component.css']
 })
 export class StationsListPageComponent implements OnInit {
+  stations : Station[];
+
   isLoading = true;
-  onlyBlocked = false;
   numberOfStations = 0;
-  currentFilterValue = "";
-  dataSource = new MatTableDataSource<Station>();
-  displayedColumns: string[] = ['name', 'status', 'availability', 'lastActivityAgo'];
 
-  @ViewChild(MatSort) sort: MatSort;
-
-  private stations : Station[];
   constructor(
     private service: CurrentStationsService) { }
 
   ngOnInit(): void {
-    this.reload();
-  }
-
-  reload(): void{
     this.isLoading = true;
-    this.dataSource = new MatTableDataSource<Station>();
     this.service.getStations()
     .subscribe((data: CurrentStations) => {
         this.stations = data.stations;
-        this.setupTable();
+        this.numberOfStations = this.stations.length;
+        this.isLoading = false;
       }
     )
-  }
-
-  filterOnlyBlocked(): void{
-    this.onlyBlocked = !this.onlyBlocked;
-    this.setupTable();
-  }
-
-  setupTable(): void{
-    var stationsToDisplay = this.stationsToDisplay(this.stations, this.onlyBlocked);
-    this.dataSource = new MatTableDataSource(stationsToDisplay);
-    this.numberOfStations = stationsToDisplay.length;
-    this.isLoading = false;
-    this.setupSort();
-    this.filter();
-  }
-
-  stationsToDisplay(stations : Station[], onlyBlocked: boolean): Station[]{
-    if(!onlyBlocked)
-      return stations;
-    
-    return stations.filter(station => station.officialStatus != "Ok" || station.state == "Locked");
-  }
-
-  setupSort(): void {
-    this.dataSource.sortingDataAccessor = (station, property) => {
-          switch(property) {
-            case 'lastActivityAgo': return station.lastActivity;
-            case 'occupation': return station.occupation;
-            default: return station[property];
-          }
-        };
-    this.dataSource.sort = this.sort;
-  }
-
-  applyFilter(event: Event) {
-    this.currentFilterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
-    this.filter();
-  }
-
-  filter(){
-    if(this.currentFilterValue.length>1 && this.currentFilterValue.length<=2)
-      return;
-    this.dataSource.filter = this.currentFilterValue;
   }
 }
