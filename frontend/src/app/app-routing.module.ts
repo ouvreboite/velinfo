36d211e74f8146cce17a6eff5ada889278ea8537
Jean-Baptiste Muscat <muscatjb@gmail.com>
2021-01-10 00:44:13 +0100

    feat: create "favorites" page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
 import { StationPageComponent } from './station-page/station-page.component';
 import { StationsListPageComponent } from './stations-list-page/stations-list-page.component';
 import { StationsMapPageComponent } from './stations-map-page/stations-map-page.component';
@@ -7,6 +8,7 @@ import { StationsMapPageComponent } from './stations-map-page/stations-map-page.
 const routes: Routes = [
   { path: 'map', component: StationsMapPageComponent },
   { path: 'list', component: StationsListPageComponent },
+  { path: 'favorites', component: FavoritesPageComponent },
   { path: 'station/:code', component: StationPageComponent },
   { path: '**', redirectTo: '/list' },
 ];
