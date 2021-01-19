b660466dca1b3a86569a376b870f142acb31093e
Jean-Baptiste Muscat <muscatjb@gmail.com>
2021-01-19 11:24:38 +0100

    feat: basic home page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { StationPageComponent } from './station-page/station-page.component';
 import { StationsListPageComponent } from './stations-list-page/stations-list-page.component';
 import { StationsMapPageComponent } from './stations-map-page/stations-map-page.component';
 
 const routes: Routes = [
   { path: 'map', component: StationsMapPageComponent },
-  { path: 'list', component: StationsListPageComponent },
+  { path: 'stations', component: StationsListPageComponent },
   { path: 'favorites', component: FavoritesPageComponent },
   { path: 'station/:code', component: StationPageComponent },
-  { path: '**', redirectTo: '/list' },
+  { path: '**', component: HomePageComponent },
 ];
 
 @NgModule({
