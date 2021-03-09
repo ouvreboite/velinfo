72c916ff9e6c2998529059f13771cb7e67b73721
Jean-Baptiste Muscat <muscatjb@gmail.com>
2021-03-09 23:04:24 +0100

    feat: create new ParisDate pipe for easier formatting

---
+++ frontend/src/app/paris-date.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'parisDate'
+})
+export class ParisDatePipe implements PipeTransform {
+  transform(date: Date): string {
+    return new Date(date).toLocaleDateString('fr-FR', { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' });
+  }
+}
